b7cff22089d096410c58313a9388e4f96a5bd235
Gregory Kikola <gkikola@gmail.com>
2017-02-26 03:39:34 -0500

    Add copyright and license information to source files

--- src/optionpp.cpp
+++ src/optionpp.cpp
@@ -1,3 +1,24 @@
+/* optionpp -- read command-line program options
+   Copyright (C) 2017 Gregory Kikola.
+
+   This file is part of option++.
+
+   option++ is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   option++ is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with option++.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/* Written by Gregory Kikola <gkikola@gmail.com>. */
+
 #include <algorithm>
 #include <cassert>
 #include <cstddef>
